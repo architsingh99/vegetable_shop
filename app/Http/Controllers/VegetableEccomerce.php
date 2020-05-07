a6c6e6608330ec5bcfdf64e7d2b029280aa49349
archit <architsingh99@gmail.com>
2020-05-07 19:53:30 +0530

    message

--- app/Http/Controllers/VegetableEccomerce.php
+++ app/Http/Controllers/VegetableEccomerce.php
@@ -13,6 +13,9 @@ use App\Pincode;
 use App\Order;
 use App\Suborder;
 
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
 use DB;
 use Hash;
 use Redirect;
@@ -23,6 +26,12 @@ use Illuminate\Http\Request;
 class VegetableEccomerce extends Controller
 {
     //
+    private $SMS_SENDER = "Sample";
+    private $RESPONSE_TYPE = 'json';
+    private $SMS_USERNAME = 'architsingh99@gmail.com';
+    private $SMS_PASSWORD = 'digboi@2014';
+
+
     public function sendResponse($data, $message) {
 
         if (count($data) > 0) {
@@ -260,10 +269,94 @@ class VegetableEccomerce extends Controller
                 $subOrder->save();
                 Cart::find($value->id)->delete();
             }
+            $this->getUserNumber($order[0]->mobile);
                return view('success')->with('transaction_id', $request['payment_id'])->with('order_id', '#'.$request->order_id)->with('delivery_time', $deliverTime);
            } 
          }catch (\Exception $e) {
             dd($e);
         }
      }
+
+
+
+     public function getUserNumber($number)
+    {
+        $phone_number = $number;
+
+        $message = "A message has been sent to you";
+
+        $this->initiateSmsActivation($phone_number, $message);
+//        $this->initiateSmsGuzzle($phone_number, $message);
+
+        return redirect()->back()->with('message', 'Message has been sent successfully');
+    }
+
+
+    public function initiateSmsActivation($phone_number, $message){
+        $isError = 0;
+        $errorMessage = true;
+
+        //Preparing post parameters
+        $postData = array(
+            'username' => $this->SMS_USERNAME,
+            'password' => $this->SMS_PASSWORD,
+            'message' => $message,
+            'sender' => $this->SMS_SENDER,
+            'mobiles' => $phone_number,
+            'response' => $this->RESPONSE_TYPE
+        );
+
+        $url = "http://portal.bulksmsnigeria.net/api/";
+
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $postData
+        ));
+
+
+        //Ignore SSL certificate verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+
+        //get response
+        $output = curl_exec($ch);
+
+
+        //Print error if any
+        if (curl_errno($ch)) {
+            $isError = true;
+            $errorMessage = curl_error($ch);
+        }
+        curl_close($ch);
+
+
+        if($isError){
+            return array('error' => 1 , 'message' => $errorMessage);
+        }else{
+            return array('error' => 0 );
+        }
+    }
+
+    public function initiateSmsGuzzle($phone_number, $message)
+    {
+        $client = new Client();
+
+        $response = $client->post('http://portal.bulksmsnigeria.net/api/?', [
+            'verify'    =>  false,
+            'form_params' => [
+                'username' => $this->SMS_USERNAME,
+                'password' => $this->SMS_PASSWORD,
+                'message' => $message,
+                'sender' => $this->SMS_SENDER,
+                'mobiles' => $phone_number,
+            ],
+        ]);
+
+
+        $response = json_decode($response->getBody(), true);
+    }
 }
