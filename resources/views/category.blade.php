59fad543b0554b0174ecaff9f20c5747a3f665ff
electronmedialab <electronmedialab@gmail.com>
2020-05-09 13:20:55 +0530

    category blade added

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,47 +1,22 @@
 @include('header')
 <!-- banner -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators-->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-        <!-- <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-			<li data-target="#myCarousel" data-slide-to="3" class=""></li> -->
-    </ol>
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h3>Fresh
-                        <span>Vegetables</span>
-                    </h3>
-                    <p>Order
-                        <span>Now</span> </p>
-                    <a class="button2" href="{{url('categories/2')}}">Shop Now </a>
-                </div>
-            </div>
-        </div>
-        <div class="item item2">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h3>Healthy
-                        <span>Fruits</span>
-                    </h3>
-                    <p>Order
-                        <span>Now</p>
-                    <a class="button2" href="{{url('categories/1')}}">Shop Now </a>
-                </div>
-            </div>
+<div class="page-head_agile_info_w3l">
+
+</div>
+<!-- //banner-2 -->
+<!-- page -->
+<div class="services-breadcrumb">
+    <div class="agile_inner_breadcrumb">
+        <div class="container">
+            <ul class="w3_short">
+                <li>
+                    <a href="{{url('/')}}">Home</a>
+                    <i>|</i>
+                </li>
+                <li>{{$msg}}</li>
+            </ul>
         </div>
     </div>
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
 </div>
 <!-- //banner -->
 
